83d14327177388d98fcd33612fd08ea8b331235d
Reepu78 <reepushah78@gmail.com>
2022-10-19 17:57:46 +0530

    first comment

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    # return HttpResponse("hello world")
+    return render(request, 'app/home.html')
+    
